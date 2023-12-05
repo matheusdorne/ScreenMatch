0bdaa24ce46929158cdbc572b220c1dbe450d652
Matheus Dornelles <matheus.dorne@gmail.com>
2023-12-04 23:14:10 -0300

    Update PrincipalComBusca.java

--- src/br/com/screenmatch/main/PrincipalComBusca.java
+++ src/br/com/screenmatch/main/PrincipalComBusca.java
@@ -1,7 +1,35 @@
 package br.com.screenmatch.main;
 
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
 public class PrincipalComBusca {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner leitura = new Scanner (System.in);
+        System.out.println("Digite um filme para busca: ");
+        var busca  = leitura.nextLine();
+       
+        String endereco = "http://www.omdbapi.com/?apikey=60826722&t=" + busca;
+        
+
+// Comunicação com webservice
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+
+
+        
 
     }
 }

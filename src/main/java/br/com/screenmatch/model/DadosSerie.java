bf56121a42f037ab8349ad0b0d1a4651d9cc10cc
Matheus Dornelles <matheus.dorne@gmail.com>
2024-01-17 17:34:55 -0300

    Update DadosSerie.java

--- src/main/java/br/com/screenmatch/model/DadosSerie.java
+++ src/main/java/br/com/screenmatch/model/DadosSerie.java
@@ -1,7 +1,9 @@
 package br.com.screenmatch.model;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record DadosSerie(@JsonAlias("Title") String titulo,
                          @JsonAlias("totalSeaons") Integer totalTemporadas,
                          @JsonAlias("imdbRating ") String avaliacao){

caa5814c32d53231206eef975182235499901838
Matheus Dornelles <matheus.dorne@gmail.com>
2024-01-29 17:17:49 -0300

    add

---
+++ src/main/java/br/com/screenmatch/model/Episodio.java
@@ -0,0 +1,55 @@
+package br.com.screenmatch.model;
+
+
+import java.time.LocalDate;
+
+public class Episodios {
+    private Integer temporada;
+    private String titulo;
+    private Integer numero;
+    private Double avaliacao;
+    private LocalDate dataLancamento;
+
+    public Episodios(int numeroTemorada, DadosEpisodio dadosEpisodio) {
+    }
+
+    public Integer getTemporada() {
+        return temporada;
+    }
+
+    public void setTemporada(Integer temporada) {
+        this.temporada = temporada;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public Double getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(Double avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public LocalDate getDataLancamento() {
+        return dataLancamento;
+    }
+
+    public void setDataLancamento(LocalDate dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+}
--- src/main/java/br/com/screenmatch/model/Episodios.java
+++
@@ -1,5 +0,0 @@
-package br.com.screenmatch.model;
-
-public class Episodios {
-    private Integer temporada;
-}

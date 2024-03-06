642b9ce7e2a9ec47e86fe0eeebecbb161648140b
Matheus Dornelles <matheus.dorne@gmail.com>
2024-03-05 23:27:26 -0300

    up

--- src/main/java/br/com/screenmatch/model/Categoria.java
+++ src/main/java/br/com/screenmatch/model/Categoria.java
@@ -1,21 +1,24 @@
 package br.com.screenmatch.model;
 
 public enum Categoria {
-    ACAO("Action"),
-    AVENTURA("Adventure"),
-    COMEDIA("Comedy"),
-    DRAMA ("Drama"),
-    FANTASIA ("Fantasy"),
-    FICCAO ("Sci-Fi"),
-    ROMANCE ("Romance"),
-    SUSPENSE ("Thriller"),
-    TERROR ("Horror"),
-    CRIME ("Crime");
+    ACAO("Action","Ação"),
+    AVENTURA("Adventure","Romance"),
+    COMEDIA("Comedy","Comédia"),
+    DRAMA ("Drama", "Drama"),
+    FANTASIA ("Fantasy","Fantasia"),
+    FICCAO ("Sci-Fi", "Ficção"),
+    ROMANCE ("Romance", "Romance"),
+    SUSPENSE ("Thriller","Suspense"),
+    TERROR ("Horror","Terror"),
+    CRIME ("Crime","Crime"),;
 
     private String categoriaOmdb;
 
-    Categoria(String categoriaOmdb) {
+    private String categoriaPortugues;
+
+    Categoria(String categoriaOmdb, String categoriaPortugues) {
         this.categoriaOmdb = categoriaOmdb;
+        this.categoriaPortugues = categoriaPortugues;
     }
 
     public static  Categoria fromString(String text) {
@@ -24,6 +27,15 @@ public enum Categoria {
                 return categoria;
             }
         }
-        throw  new IllegalArgumentException("Nenehuma ctegoria encontrada para essa serie");
+        throw  new IllegalArgumentException("Nenhuma categoria encontrada para essa serie");
+    }
+
+    public static  Categoria fromPortugues(String text) {
+        for (Categoria categoria : Categoria.values()) {
+            if (categoria.categoriaPortugues.equalsIgnoreCase(text)) {
+                return categoria;
+            }
+        }
+        throw  new IllegalArgumentException("Nenhuma categoria encontrada");
     }
 }
